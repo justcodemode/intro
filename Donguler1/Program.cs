44f3abfd96b6050b1fa51ed14a277205aba53367
sıla taş <silatass@gmail.com>
2021-01-31 17:51:59 +0300

    23 january / interface

---
+++ Donguler1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Donguler1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string kurs1 = "Yazılım Gelistirici Yetistirme Kampı";
+			string kurs2 = "Programlamaya baslangic icin temel kurs";
+			string kurs3 = "Java";
+
+			string[] kurslar = new string[] { "Yazılım Gelistirici Yetistirme Kampı", "Programlamaya baslangic icin temel kurs", "Java", "C#" };
+
+
+			
+			for (int i = 0; i < kurslar.Length; i++)
+			{
+				Console.WriteLine(kurslar[i]);
+			}
+
+			Console.WriteLine("For bitti");
+
+			foreach (string kurs in kurslar)
+			{
+				Console.WriteLine(kurs);
+			}
+			Console.WriteLine("sayfa sonu - footer");
+		}
+	}
+}
---
+++ Kampİntro1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Kampİntro1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//type safety - tip güvenliği
+			//Do not repeat yourself - Kendini tekrarlama
+			//değer tutucu, alias
+
+			string kategoriEtiketi = "Kategoriler";
+			int ugrenciSayisi = 32000;
+			double faizOrani = 1.45;
+			bool sistemeGirisYapmisMi = true;
+			double dolarDun = 7.35;
+			double dolarBugun = 7.45;
+
+			if (dolarDun>dolarBugun)
+			{
+				Console.WriteLine("Azalış butonu");
+			}
+			else if (dolarDun<dolarBugun) 
+			{
+				Console.WriteLine("Azalış butonu");
+			}
+			else
+			{
+				Console.WriteLine("değişmediButonu");
+			}
+			if (sistemeGirisYapmisMi == true)
+			{
+				Console.WriteLine("Kullanıcı ayarları butonu");
+			}
+			else
+			{
+				Console.WriteLine("Giris yap butonu");
+			}
+
+
+
+			Console.WriteLine(kategoriEtiketi);
+		
+
+			
+
+		}
+
+
+	}
+
+} 
+
+
+
+
+
+			
+
+
+
+
+
